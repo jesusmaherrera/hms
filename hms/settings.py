533d04725f7f6899b78e0e0470cb3fa84839b7a5
jesusmaherrera <jesusmaherrera@gmail.com>
2012-09-04 10:11:04 -0600

    algo

--- hms/settings.py
+++ hms/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'hms_db',                      # Or path to database file if using sqlite3.
+        'NAME': 'C:\django_bd\hms_db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
